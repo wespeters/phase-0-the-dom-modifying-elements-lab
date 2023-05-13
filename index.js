9f3ab6780c0b97fd247f0bdcd6b947dd6bc48f3d
Wesley Peters <wesleypeters@gmail.com>
2023-05-13 16:05:57 -0600

    removes and inserts required DOM nodes

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "Wes is the champion";
+
+
